c50ee1b249186eaefc3a66b727948276f1bc7883
Paulo Carvalho <pauloej.carvalho@gmail.com>
2022-05-28 14:32:22 -0300

    feat(configs): adding initial settings and Header component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route} from 'react-router-dom'
+
+import { makeStyles } from '@material-ui/core';
+
 import './App.css';
 
+import Header from './components/Header';
+import CoinPage from './pages/CoinPage';
+import Homepage from './pages/Homepage';
+
 function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "black",
+      color: "white",
+      minHeight: "100vh",
+    }
+  }));
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { 
+  AppBar, 
+  Container, 
+  MenuItem, 
+  Select, 
+  Toolbar, 
+  Typography,
+  makeStyles,
+  createTheme,
+  ThemeProvider,
+} from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "#8257e5",
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+  selectCurrency: {
+    width: 100,
+    height: 40,
+    marginRight: 15,
+  }
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color='transparent' position='static'>
+        <Container>
+          <Toolbar>
+            <Typography 
+              variant="h6"
+              className={classes.title} 
+              onClick={() => history.push("/")}
+            >
+              Crypto Market
+            </Typography>
+            <Select variant="outlined" className={classes.selectCurrency}>
+              <MenuItem value="USD">USD</MenuItem>
+              <MenuItem value="BRL">BRL</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  )
+}
+
+export default Header
