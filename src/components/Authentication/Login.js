840325cca8aaad708d25b6096a805185c40b15c4
Paulo Carvalho <pauloej.carvalho@gmail.com>
2022-06-02 11:08:17 -0300

    feat(login): including Login component

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Button, makeStyles, TextField } from '@material-ui/core';
 
 import { useCryptoContext } from '../../CryptoContext';
 
+const useStyles = makeStyles(() => ({
+  boxContainer: {
+   display: "flex",
+   flexDirection: "column",
+   gap: "20px",
+  },
+}));
+
 const Login = () => {
+  const classes = useStyles();
   const { handleVisibleModal } = useCryptoContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+
+  };
 
   return (
-    <div>Login</div>
+    <Box p={3} className={classes.boxContainer}>
+      <TextField 
+        variant="outlined"
+        type="email"
+        label="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        fullWidth
+      />
+      <TextField 
+        variant="outlined"
+        type="password"
+        label="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        fullWidth
+      />
+
+      <Button
+        variant="contained"
+        size="large"
+        color="primary"
+        onClick={handleSubmit}
+      >
+        Login
+      </Button>
+    </Box>
   )
 }
 
